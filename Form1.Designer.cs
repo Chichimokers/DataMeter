a1ba250436117ada9ad41966b96c9c858c5f552e
Ernesto Rodriguez Castillo <developer1575@gmail.com>
2025-01-08 16:52:16 -0500

    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Data_Counter
+{
+    partial class Datametera
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Datametera));
+            this.t = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // t
+            // 
+            this.t.AutoSize = true;
+            this.t.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t.Location = new System.Drawing.Point(69, 49);
+            this.t.Name = "t";
+            this.t.Size = new System.Drawing.Size(56, 25);
+            this.t.TabIndex = 0;
+            this.t.Text = "0MB";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(74, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Datametera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 111);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.t);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Datametera";
+            this.Text = "Datameter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label t;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace Data_Counter
+{
+    public partial class Datametera : Form
+    {
+        private Timer timer;
+        private NetworkInterface selectedNetworkInterface;
+        private long initialBytesSent;
+        private long initialBytesReceived;
+
+        public Datametera()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 1000; // Actualizar cada 1 segundo
+            timer.Tick += Timer_Tick;
+        }
+
+        public string[] GetAllNetworkInterfaceNames()
+        {
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            string[] interfaceNames = new string[interfaces.Length];
+
+            for (int i = 0; i < interfaces.Length; i++)
+            {
+                interfaceNames[i] = interfaces[i].Name;
+            }
+
+            return interfaceNames;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] networkInterfaces = GetAllNetworkInterfaceNames();
+
+            if (networkInterfaces.Length > 0)
+            {
+                comboBox1.Items.AddRange(networkInterfaces);
+                comboBox1.SelectedIndex = 0; // Selecciona el primer adaptador por defecto
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedInterfaceName = comboBox1.SelectedItem.ToString();
+            selectedNetworkInterface = GetNetworkInterfaceByName(selectedInterfaceName);
+
+            if (selectedNetworkInterface != null)
+            {
+                // Inicializa los valores de tráfico
+                IPv4InterfaceStatistics stats = selectedNetworkInterface.GetIPv4Statistics();
+                initialBytesSent = stats.BytesSent;
+                initialBytesReceived = stats.BytesReceived;
+
+                // Reinicia el contador y arranca el temporizador
+                t.Text = "0 MB";
+                timer.Start();
+            }
+            else
+            {
+                timer.Stop();
+                t.Text = "No se puede monitorear.";
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (selectedNetworkInterface != null)
+            {
+                IPv4InterfaceStatistics stats = selectedNetworkInterface.GetIPv4Statistics();
+
+                // Calcula el tráfico actual desde que se inició el programa
+                long currentBytesSent = stats.BytesSent - initialBytesSent;
+                long currentBytesReceived = stats.BytesReceived - initialBytesReceived;
+                long totalBytes = currentBytesSent + currentBytesReceived;
+
+                // Convierte a MB y actualiza el label
+                double totalMb = totalBytes / 1024.0 / 1024.0;
+                t.Text = $"{totalMb:F2} MB";
+            }
+        }
+
+        private NetworkInterface GetNetworkInterfaceByName(string name)
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.Name == name)
+                {
+                    return ni;
+                }
+            }
+            return null;
+        }
+    }
+}
